5ab51e09313bb7555e6e23e0b28624e5b3d77cc8
Demóstenes Acoglanis <126527444+Ademostenes@users.noreply.github.com>
2023-04-14 00:02:10 -0300

    Add files via upload

---
+++ PronostiCsv.java
@@ -0,0 +1,14 @@
+
+package PronostiCsv;
+
+public class PronostiCsv {
+    public static void main(String[] args) {
+         Menu.cargarPuntos(); // Calcula puntaje y lo asigna a cada jugador. Ver clase Menu.
+         Menu.listarJugXPuntos();
+         System.out.println();
+         System.out.println("Gracias por utilizar el programa");
+     
+	    
+	    }
+    
+}
